67c128848634e757b470651e12651dcaa1ba1bb6
DESKTOP\Yannic <yapeulet@gmx.de>
2020-12-06 14:40:00 +0100

    Day 06

---
+++ 06/06.py
@@ -0,0 +1,21 @@
+import re
+
+if __name__ == '__main__':
+
+       with open('./06/06.in') as file:
+        groups = [x for x in file.read().split("\n\n")]
+        
+        # Part One
+        result = 0
+        for group in groups:
+            group_yes = set(group.replace("\n", ""))
+            result += len(group_yes)
+        print(result)
+
+        # Part Two
+        result = 0
+        for group in groups:
+            members = [set(x) for x in group.split("\n")]
+            group_yes = members[0].intersection(*members)
+            result += len(group_yes)
+        print(result)
