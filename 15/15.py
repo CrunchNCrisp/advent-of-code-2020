803cfd116e93805973e066b59b30bdd6bec665f3
CrunchNCrisp <Yannic.Behrendt@bosch.io>
2020-12-15 08:48:51 +0100

    Day 15

--- 15/15.py
+++ 15/15.py
@@ -1,4 +1,26 @@
 if __name__ == '__main__':
 
-    with open('./14/14.in') as file:
-        instructions = [x.split(" = ") for x in file.read().splitlines()]
+    with open('./15/15.in') as file:
+        numbers = [int(x) for x in file.readline().split(",")]
+        last_index = {}
+
+        for i,n in enumerate(numbers):
+            if i != len(numbers[:-1]):
+                last_index[n] = i
+
+        while len(numbers) < 30000000:
+            last = numbers[-1]
+            prev_last = last_index.get(last, -1)
+            last_index[last] = len(numbers[:-1])
+            if prev_last == -1:
+                new_number = 0
+            else:
+                new_number = len(numbers[:-1]) - prev_last
+            numbers.append(new_number)
+            
+            # Part One
+            if len(numbers) == 2020:
+                print(new_number)
+        
+        # Part Two
+        print(numbers[-1])
